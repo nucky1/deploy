f7b73baa18f65493daa322129c6bc96e62eb879d
nucky1 <51407707+nucky1@users.noreply.github.com>
2023-11-28 01:13:00 -0300

    Update docker.sh

--- fronts/okmotos/docker.sh
+++ fronts/okmotos/docker.sh
@@ -98,6 +98,8 @@ fi
 # Continuar con el resto del contenido de configuración
 archivo="$archivo
 
+RUN npm run build
+
 # Production image, copy all the files and run next
 FROM base AS runner
 WORKDIR /app
@@ -107,15 +109,6 @@ ENV NODE_ENV production
 RUN addgroup --system --gid 1001 nodejs
 RUN adduser --system --uid 1001 nextjs
 
-# Set the correct permission for prerender cache
-RUN mkdir .next
-RUN chown nextjs:nodejs .next
-
-COPY --from=builder --chown=nextjs:nodejs /app/public ./public
-
-# Automatically leverage output traces to reduce image size
-# https://nextjs.org/docs/advanced-features/output-file-tracing
-COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
 
 USER nextjs
 
